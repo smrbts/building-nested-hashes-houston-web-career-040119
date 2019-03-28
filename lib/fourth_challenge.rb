ef751e2ff07ea879aedc572b655c376e040c4c07
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 16:16:21 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,8 +20,16 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        #your hashes here!
+      :heroine_friends => 
+      [
+         { name: "Steven",
+          age: "30",
+          attitude: "confused"
+        },
+        { name: "Nurse",
+          age: "18",
+          attitude: "hot-headed"
+        }
       ]
    }
 }
